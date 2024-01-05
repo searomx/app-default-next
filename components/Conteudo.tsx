cb6bc6381b5df3d0d45a84b392fb4cad5a1d568c
searomx <searomx@gmail.com>
2024-01-05 09:19:38 -0300

    releases 050124-0919

--- components/Conteudo.tsx
+++ components/Conteudo.tsx
@@ -14,28 +14,26 @@ export default function Conteudo() {
   return (
     <section>
       <div className="flex flex-col w-full min-h-[calc(100vh_-_14.5rem)] p-3">
-        <div className="flex w-full">
-          <Grid xl2={2} sm={1} md={2} lg={2} xl={2}>
-            <div
-              className={`flex-1
+        <div className="flex justify-between">
+          <div
+            className={`flex-initial
                       border border-solid border-zinc-400
                       max-h-[calc(100vh_-_14.8rem)]
                       rounded-md overflow-hidden
-                      p-2 w-full bg-amber-400`}
-            >
-              <TableCnpjBase cnpj={ListaCnpjBase} />
-            </div>
+                      p-2  bg-amber-400`}
+          >
+            <TableCnpjBase cnpj={ListaCnpjBase} />
+          </div>
 
-            <div
-              className={`flex-3
+          <div
+            className={`flex-1
                     border border-solid border-zinc-400
                     max-h-[calc(100vh_-_14.8rem)]
                     rounded-md
-                    p-2 w-full bg-amber-400`}
-            >
-              <TableDadosClientes clientes={ListaClientes} />
-            </div>
-          </Grid>
+                    p-2 bg-amber-400`}
+          >
+            <TableDadosClientes clientes={ListaClientes} />
+          </div>
         </div>
       </div>
     </section>
--- components/TableCnpjBase.tsx
+++ components/TableCnpjBase.tsx
@@ -6,20 +6,20 @@ interface IdadosCnpjProps {
 
 export default function TableCnpjBase(props: IdadosCnpjProps) {
   return (
-    <table className="flex flex-col min-w-full h-full scroll-auto">
+    <table className="flex flex-col scroll-auto p-4">
       <thead>
         <tr className="flex min-w-full justify-center">
-          <th>ID</th>
-          <th className="flex w-full justify-center bg-cyan-100">CNPJ-BASE</th>
+          <th className="w-[20%]">ID</th>
+          <th className="flex w-[80%] justify-center bg-cyan-100">CNPJ-BASE</th>
         </tr>
       </thead>
       <tbody>
         {props.cnpj.map((cnpj, index) => (
           <tr key={cnpj} className="flex min-w-full">
-            <td className="text-black font-bold justify-center items-center mx-1">
+            <td className="text-black font-bold justify-center items-center mx-2 w-[20%]">
               {index + 1}
             </td>
-            <td className="text-black font-bold justify-center items-center">
+            <td className="text-black font-bold justify-center items-center w-[80%] mx-2">
               {cnpj.cnpj}
             </td>
           </tr>
